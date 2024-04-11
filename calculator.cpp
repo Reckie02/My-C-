5814837e334d7bbab9b658ec20d2c70b556a3ab3
Recky Oluoch <119103415+Reckie02@users.noreply.github.com>
2024-04-11 11:56:01 +0300

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	//define Variables
+	int a,b,add,sub,mul,div;
+	
+	//get the user Input
+	cout<<"Enter first Number:"<<endl;
+	cin >> a;
+	cout <<" Enter Second Number: " <<endl;
+	cin >> b;
+	
+	//Performing Operations
+	add=a+b;
+	cout<<"The sum is: "<<add<<endl;
+	
+	sub=a-b;
+	cout<<"The difference is: "<<sub<<endl;
+	
+	mul=a*b;
+	cout<<"The Product is: "<<mul<<endl;
+	
+	
+	div=a/b;
+	cout<<"The Dividend is: "<<div<<endl;
+	
+
+	
+}
